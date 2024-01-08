1e2697eb548ada8493027b5070020c42594a37d5
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-01-08 14:12:36 -0600

    Create base files to simplify installation

---
+++ runner.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+
+sudo apt install avahi-daemon -y
+# Ensure version 0.8-10 is installed
+
+sudo apt install vim -y
+# Ensure version 9.0.1378-2 is installed
+
+sudo apt install python3-defaults -y
+# Ensure version 3.11.2-1+b1
